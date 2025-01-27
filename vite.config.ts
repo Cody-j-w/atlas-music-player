2a59b6e9709999f079144ad0e50df52ec05b4513
Cody Walenciak <codywalenciak@gmail.com>
2025-01-27 13:30:40 -0600

    Add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+/// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+
+  }
 });
